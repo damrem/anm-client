fbd01853dcfe93694cee85508422938b09a0642a
damrem <damien.remars@gmail.com>
2014-09-15 22:47:44 +0200

    log the static path in order to debug what's happening in prod

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-var application_root=__dirname;
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var errorHandler=require("errorhandler");
-var path=require("path");
+'use strict';
+
+var applicationRoot=__dirname;
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler=require('errorhandler');
+//var html=require('html');
+var path=require('path');
 
 var app=express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(express.static(path.join(application_root, "dist/")));
+console.log('path.join: '+path.join(applicationRoot, 'dist/'));
+//console.log('express.static'+express.static(path.join(applicationRoot, 'dist/')));
+app.use(express.static(path.join(applicationRoot, 'dist/')));
 app.use(errorHandler({dumpExceptions:true, showStack:true}));
 
 app.set('port', (process.env.PORT || 5000));
 console.log('port: '+app.get('port'));
-app.set('view engine', 'html');
+//app.set('view engine', 'html');
+
 /*
 app.get('/', function (req, res){
-	res.render('client/');
+
+	res.render('../dist/index.html');
 });
 */
 
+
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
