95b467819fc820040056db4bc4a8e84896b521e3
damrem <damien.remars@gmail.com>
2014-09-30 09:03:34 +0200

    jshint back to project root for sublime editor

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,7 +118,7 @@ module.exports = function (grunt) {
     // Make sure code styles are up to par and there are no obvious mistakes
     jshint: {
       options: {
-        jshintrc: 'conf/.jshintrc',
+        jshintrc: '.jshintrc',
         reporter: require('jshint-stylish')
       },
       all: {
@@ -129,7 +129,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          jshintrc: 'conf/.jshintrc'
+          jshintrc: '.jshintrc'
         },
         src: '<%= yeoman.app %>/**/{,*/}*_test.js'
       }
